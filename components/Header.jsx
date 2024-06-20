b3faa9f5507996d76bae857af4c905d382432b4c
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-20 09:41:26 +0200

    ADD: Componente header

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+"use client"
+import { useEffect } from "react"
+
+function Header({ isPage }) {
+  useEffect(() => {
+    require("../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src="./header-images/logo.webp" width={32} height={25} alt="" />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className={isPage == "home" ? "nav-link active" : "nav-link"} aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className={isPage == "about" ? "nav-link active" : "nav-link"} href="#">Chi siamo</a>
+            </li>
+            <li className="nav-item">
+              <a className={isPage == "news" ? "nav-link active" : "nav-link"} href="#">News</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Discord</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/app/test/page.js
@@ -0,0 +1,16 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+import styles from "./page.module.css";
+import Header from "../../../components/Header";
+
+export default function Home() {
+  return (
+    <>
+    <main>
+      <Header isPage="home"></Header>
+      <h1>test</h1>
+    </main>
+    
+    </>
+  );
+}
