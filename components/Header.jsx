3bb9961f54115d8681f8663b01d3436f08fe5fd3
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-20 22:12:34 +0200

    FIX: Fixato load immagini con Image di nextjs

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { useEffect } from "react"
+import Image from "next/image.js";
 
 function Header({ isPage }) {
   useEffect(() => {
@@ -11,7 +12,7 @@ function Header({ isPage }) {
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div className="container">
           <a className="navbar-brand" href="/">
-            <img src="./header-images/logo.webp" width={32} height={25} alt="" />
+            <Image src="/header-images/logo.webp" width={32} height={25} alt="Logo di XDefiant Italia" />
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
