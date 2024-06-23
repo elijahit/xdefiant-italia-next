ee2f9c055ada6717eeaf77f32fe87832b5c958ce
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-23 23:23:13 +0200

    ADD: layout per discord page e news page

---
+++ src/app/discord/layout.js
@@ -0,0 +1,39 @@
+// import { Inter } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import "./globals.css";
+import siteConfig from "../../siteConfig.json"
+
+
+export const metadata = {
+  title: "XDefiant Italia - Discord",
+  openGraph: {
+    title: 'XDefiant Italia',
+    description: 'Community italiana di XDefiant dal 2021. Su discord aggiorniamo i giocatori su novità, eventi e competizioni. Unisciti a noi per condividere esperienze, strategie e partecipare a tornei',
+    url: siteConfig.site.baseUri,
+    siteName: 'XDefiant Italia - Community',
+    images: [
+      {
+        url: `${siteConfig.site.baseUri}metadata/backgroundOg.jpg`, // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: `${siteConfig.site.baseUri}metadata/backgroundOg.jpg`, // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Background XDefiant Italia',
+      },
+    ],
+    locale: 'it_IT',
+    type: 'website',
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
