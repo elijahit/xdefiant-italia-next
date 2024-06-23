acde37307836f438f2ca8f9ebfa0e17bbc446aab
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-23 10:30:40 +0200

    FIX: Footer responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,10 +7,10 @@ function Footer() {
       <div className="container">
         {/* FOOTER PER DESKTOP */}
         <div className="row">
-          <div className="col-12 mt-5 mb-4">
+          <div className="col-12 mt-5 mb-4 d-flex justify-content-center d-lg-block">
             <Image src={"/footer-images/logo.webp"} alt="Logo XDefiant Italia" width={20} height={20}></Image>
           </div>
-          <div className="col-4">
+          <div className="col-4 d-none d-lg-block">
             <h6>Navigazione</h6>
             <ul className="list-unstyled">
               <li>
@@ -21,29 +21,29 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="col-8">
-            <h6>Informazioni</h6>
-            <p>playxdefiant.it non è in nessun modo affiliato a <b>Ubisoft Entertainment</b></p>
-            <p>
+          <div className="col-12 col-lg-8">
+            <h6 className="text-center text-lg-start">Informazioni</h6>
+            <p className="text-center text-lg-start">playxdefiant.it non è in nessun modo affiliato a <b>Ubisoft Entertainment</b></p>
+            <p className="text-center text-lg-start">
               Developed by: <a href="https://www.linkedin.com/in/gabriele-tosto/"><u>Gabriele Mario Tosto</u></a> <br />
-              Design by: <a href="https://www.behance.net/gabuzzinialessio"><u>Gabbuzzini Alessio</u></a>
+              Design by: <a href="https://www.behance.net/gabuzzinialessio"><u>Gabuzzini Alessio</u></a>
             </p>
           </div>
-          <div className="col-12 d-flex flex-column align-items-end mb-5">
+          <div className="col-12 d-flex flex-column align-items-center align-items-lg-end mb-5">
             <div className="d-flex gap-3">
               <div className="social-background">
                 <a href="https://x.com/playxdefiant_it" target="_blank">
-                  <i class="bi bi-twitter-x"></i>
+                  <i className="bi bi-twitter-x"></i>
                 </a>
               </div>
               <div className="social-background">
                 <a href="https://www.instagram.com/playxdefiant_IT/" target="_blank">
-                  <i class="bi bi-instagram"></i>
+                  <i className="bi bi-instagram"></i>
                 </a>
               </div>
               <div className="social-background">
                 <a href="https://www.facebook.com/PlayXDefiantIT/" target="_blank">
-                  <i class="bi bi-facebook"></i>
+                  <i className="bi bi-facebook"></i>
                 </a>
               </div>
             </div>
