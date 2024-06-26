48c179652e0e0c953c31a9064b5ee0801df3876a
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-26 06:34:35 +0200

    FIX: Reactivity with db

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -7,11 +7,11 @@ import CardNewsHeader from "../../../components/CardNewsHeader"
 import { notFound } from "next/navigation";
 import db from "../../../scripts/database";
 
-export default async function Home() {ù
+export default async function Home() {
   
   let postData = await db.all("select a.id_article , a.titolo, a.testo, a.id_utente, a.image_url, a.uri_article, u.username, u.email, u.discord_name from article a join utente u on u.id_utente = a.id_utente ORDER BY a.id_article DESC LIMIT ?", 2);
   let postDataAll = await db.all("select a.id_article , a.titolo, a.testo, a.id_utente, a.image_url, a.uri_article, u.username, u.email, u.discord_name from article a join utente u on u.id_utente = a.id_utente ORDER BY a.id_article DESC");
-  
+
   return (
     <>
       {/* HEADER */}
