b7faa8710261e49fc1ae4ac27c3cb841c49ef38d
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-26 22:13:37 +0200

    ADD: CSP Rigoroso

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig = {
   async redirects() {
     return [
@@ -25,6 +38,10 @@ const nextConfig = {
                 { key: "Access-Control-Allow-Origin", value: "https://playxdefiant.it" }, // Sostituisci con il tuo dominio effettivo
                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                {
+                  key: 'Content-Security-Policy',
+                  value: cspHeader.replace(/\n/g, ''),
+                },
             ]
         }
     ];
