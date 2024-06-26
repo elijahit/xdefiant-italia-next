49f8dc42b2693da54413caf498beb898aee64792
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-26 06:15:52 +0200

    FIX: Reactivity with db

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Hero from "../../components/Hero";
 import Image from "next/image";
 import CardNewsHeader from "../../components/CardNewsHeader"
 import { notFound } from "next/navigation";
+import db from "../../scripts/database";
 
 export default async function Home() {
   let postData = await db.all("select a.id_article , a.titolo, a.testo, a.id_utente, a.image_url, a.uri_article, u.username, u.email, u.discord_name from article a join utente u on u.id_utente = a.id_utente ORDER BY a.id_article DESC LIMIT ?", 2);
