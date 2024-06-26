3378580a070d35666bab14e17a51eb1806085c23
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-26 06:17:42 +0200

    FIX: Reactivity with db

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,7 @@ export default async function Home() {
             <h3 className="fs-5 text-center mb-4">LE ULTIME NEWS</h3>
             <div className="flex-column flex-lg-row d-flex gap-5 justify-content-center align-items-center mb-5">
               {postData.map((object, i) => <CardNewsHeader key={i} image={object.image_url} title={object.titolo.length > 49 ? object.titolo.slice(0, 49)+"..." : object.titolo} uri={object.uri_article}/>)}
+              {db.close()}
             </div>
             <a href="/news"  className="btn btn-outline-light">Visualizza tutti i nostri articoli</a>
           </div>
