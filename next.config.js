2af33048c7ed4b954f9fffb24dcb4779fbb24dbb
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-26 20:21:32 +0200

    ADD: Funcs approva post

--- next.config.js
+++ next.config.js
@@ -15,14 +15,20 @@ const nextConfig = {
       },
     ]
   },
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://playxdefiant.it/:path*',
-      },
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "playxdefiant.it" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
-  },
+  }
 }
 
 module.exports = nextConfig
