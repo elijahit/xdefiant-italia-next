e4298a65153edc0ec3fd6d4956415a954d62d7d0
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-28 20:57:06 +0200

    ADD Modal.jsx components

--- components/AdminCommandsPosts.jsx
+++ components/AdminCommandsPosts.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import "./AdminCommandsPosts.css";
+import Modal from "./Modal";
 
 
 export default function AdminCommands({ adminLevel, author, author_id, request_username, request_id, post_id }) {
@@ -41,31 +42,13 @@ export default function AdminCommands({ adminLevel, author, author_id, request_u
             <button className="button-custom"><i className="bi bi-pencil-square"></i> Modifica articolo</button>
           </div>
           <div className="col-6 d-flex justify-content-start">
-            <button data-bs-toggle="modal" data-bs-target="#myModal" className="button-custom"><i className="bi bi-backspace"></i> Elimina articolo</button>
+            <button data-bs-toggle="modal" data-bs-target="#modalConfirmCancel" className="button-custom"><i className="bi bi-backspace"></i> Elimina articolo</button>
           </div>
         </div>
         : ""}
 
       {/* MODALE */}
-      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Modal id="modalConfirmCancel" titolo="ATTENZIONE!!!" testo="Stai cercando di eliminare un articolo, sei sicuro di volerlo fare ?" />
     </>
   );
 }
---
+++ components/Modal.jsx
@@ -0,0 +1,22 @@
+import "./Modal.css";
+
+export default function Modal({titolo, testo, id, funcs}) {
+  return (
+    <div className="modal fade text-white" id={id} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog modal-custom" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">{titolo}</h5>
+          </div>
+          <div className="modal-body">
+            {testo}
+          </div>
+          <div className="modal-footer">
+            <button type="button" onClick={() => {funcs}} className="btn btn-si">SI</button>
+            <button type="button" className="btn btn-no" data-bs-dismiss="modal">NO</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
