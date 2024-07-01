e3bd9d274b5cc75697c1685a338f758a6f6ad942
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-01 21:41:24 +0200

    ADD: Next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.SITE_URL || 'https://playxdefiant.it/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
