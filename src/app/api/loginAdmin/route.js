756ad7dcebf3286cf780a47d414783e4ce937ffa
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-07 11:13:10 +0200

    ADD: Insert in login admin

--- src/app/api/loginAdmin/route.js
+++ src/app/api/loginAdmin/route.js
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
 import db from "../../../../scripts/database";
+import { headers } from "next/headers";
 
 // To handle a GET request to /api
 export async function GET(request) {
   const searchParams = request.nextUrl.searchParams
   let email = searchParams.get('email')
   let token = searchParams.get('authToken')
+  
+  const headersList = headers();
+  const ip = headersList.get("x-forwarded-for");
 
   let res = await db.get("SELECT * FROM utente WHERE email = ? AND authorization_token = ?", email, token);
   const originalUrl = request.nextUrl.protocol + request.headers.get('host') + request.nextUrl.pathname
@@ -15,6 +19,7 @@ export async function GET(request) {
     let response = NextResponse.redirect(postArticle);
     response.cookies.set('email', email);
     response.cookies.set('authToken', token);
+    db.run("INSERT INTO admin_logs (id_utente, azione, timestamp, ip) VALUES(?, 1, ?, ?)", res.id_utente, new Date().getTime(), ip);
     return response;
   } else {
     return NextResponse.redirect(errorLogin);
