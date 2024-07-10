2111489beb873e933d32027502d1ef5b715d4945
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-10 17:05:23 +0200

    ADD: Schema.org

--- src/app/news/layout.js
+++ src/app/news/layout.js
@@ -1,36 +1,34 @@
-import { schemaSite } from "../layout";
-
 export const metadata = {
   title: "XDefiant Italia - News",
 };
 
-schemaSite = {
-  "@context": "https://schema.org",
-  "@type": "WebPage",
-  "url": "https://playxdefiant.it/news",
-  "image": '/header-images/logo.webp',
-  "name": "XDefiant Italia - News",
-  "description": "Notizie sul gioco di XDefiant e approfondimenti forniti dalla redazione di XDefiant Italia",
-  "isPartOf": {
-    "@type": "WebSite",
-    "url": "https://playxdefiant.it",
-    "name": "XDefiant Italia"
-  },
-  "publisher": {
-    '@type': 'Organization',
-    'name': 'XDefiant Italia',
-    'logo': '/header-images/logo.webp',
-    'keywords': 'xdefiant italia, xdefiant, news xdefiant italia, news, tornei, community, discord',
-    'founder': [{
-      '@type': 'Person',
-      'name': 'Gabriele Mario Tosto',
-      'description': 'CEO e Developer di XDefiant Italia, lavora attualmente come sviluppatore attivo.',
-      'jobTitle': 'Software Engineer',
-      'givenName': 'Gabriele',
-      'email': 'gabriele.tosto@outlook.com'
-    }]
-  }
-}
+// schemaSite = {
+//   "@context": "https://schema.org",
+//   "@type": "WebPage",
+//   "url": "https://playxdefiant.it/news",
+//   "image": '/header-images/logo.webp',
+//   "name": "XDefiant Italia - News",
+//   "description": "Notizie sul gioco di XDefiant e approfondimenti forniti dalla redazione di XDefiant Italia",
+//   "isPartOf": {
+//     "@type": "WebSite",
+//     "url": "https://playxdefiant.it",
+//     "name": "XDefiant Italia"
+//   },
+//   "publisher": {
+//     '@type': 'Organization',
+//     'name': 'XDefiant Italia',
+//     'logo': '/header-images/logo.webp',
+//     'keywords': 'xdefiant italia, xdefiant, news xdefiant italia, news, tornei, community, discord',
+//     'founder': [{
+//       '@type': 'Person',
+//       'name': 'Gabriele Mario Tosto',
+//       'description': 'CEO e Developer di XDefiant Italia, lavora attualmente come sviluppatore attivo.',
+//       'jobTitle': 'Software Engineer',
+//       'givenName': 'Gabriele',
+//       'email': 'gabriele.tosto@outlook.com'
+//     }]
+//   }
+// }
 
 export default function RootLayout({ children }) {
   return (
