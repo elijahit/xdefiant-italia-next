bfd9b8e5d7e7076ffb65c79dab5c6ad541254d9b
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-11 18:29:45 +0200

    INIT: Iniziato a lavorare su tracker di XDefiant

---
+++ src/app/tracker/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "XDefiant Italia - Tracker",
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
