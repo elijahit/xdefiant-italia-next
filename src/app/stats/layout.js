2cb5bdef03910284902a19b8a1793ded09648351
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-14 09:23:42 +0200

    ADD: metadata in layout di stats

--- src/app/stats/layout.js
+++ src/app/stats/layout.js
@@ -1,3 +1,5 @@
+import siteConfig from "../../../siteConfig.json"
+
 export const metadata = {
   title: "XDefiant Italia - Tracker",
   description: "Tracker per le tue statistiche di XDefiant in Italiano",
