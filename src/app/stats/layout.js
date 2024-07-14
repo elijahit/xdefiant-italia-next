7a01ee5a13030b4558e80767d150c14912f40d4c
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-14 09:22:05 +0200

    ADD: metadata in layout di stats

--- src/app/stats/layout.js
+++ src/app/stats/layout.js
@@ -1,6 +1,28 @@
 export const metadata = {
   title: "XDefiant Italia - Tracker",
   description: "Tracker per le tue statistiche di XDefiant in Italiano",
+  keywords: ['xdefiant tracker', 'xdefiant italia', 'ubisoft italia', 'ubisoft', 'xdefiant stats'],
+  openGraph: {
+    title: 'XDefiant Italia - Tracker',
+    description: 'Tracker per le tue statistiche di XDefiant in Italiano',
+    url: siteConfig.site.baseUri,
+    siteName: 'XDefiant Italia',
+    images: [
+      {
+        url: `${siteConfig.site.baseUri}metadata/backgroundOg.jpg`, // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: `${siteConfig.site.baseUri}metadata/backgroundOg.jpg`, // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Background XDefiant Italia',
+      },
+    ],
+    locale: 'it_IT',
+    type: 'website',
+  }
 };
 
 
