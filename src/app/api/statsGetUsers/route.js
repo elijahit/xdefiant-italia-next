206074ad0aade6159159cd46f1724f1175237668
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-14 15:41:35 +0200

    COMMENT: Commentate le righe sul rate limit delle API

--- src/app/api/statsGetUsers/route.js
+++ src/app/api/statsGetUsers/route.js
@@ -29,18 +29,19 @@ export async function GET(request) {
     next: { revalidate: 60 }
   })
     .then(response => {
-      if (response.headers.has('Retry-After')) {
-        console.log('Retry-After:', response.headers.get('Retry-After'));
-      }
-      if (response.headers.has('X-RateLimit-Limit')) {
-        console.log('X-RateLimit-Limit:', response.headers.get('X-RateLimit-Limit'));
-      }
-      if (response.headers.has('X-RateLimit-Remaining')) {
-        console.log('X-RateLimit-Remaining:', response.headers.get('X-RateLimit-Remaining'));
-      }
-      if (response.headers.has('X-RateLimit-Reset')) {
-        console.log('X-RateLimit-Reset:', response.headers.get('X-RateLimit-Reset'));
-      }
+      // TEST PER API RATING
+      // if (response.headers.has('Retry-After')) {
+      //   console.log('Retry-After:', response.headers.get('Retry-After'));
+      // }
+      // if (response.headers.has('X-RateLimit-Limit')) {
+      //   console.log('X-RateLimit-Limit:', response.headers.get('X-RateLimit-Limit'));
+      // }
+      // if (response.headers.has('X-RateLimit-Remaining')) {
+      //   console.log('X-RateLimit-Remaining:', response.headers.get('X-RateLimit-Remaining'));
+      // }
+      // if (response.headers.has('X-RateLimit-Reset')) {
+      //   console.log('X-RateLimit-Reset:', response.headers.get('X-RateLimit-Reset'));
+      // }
       return response.json()
     })
     .then(async data => {
