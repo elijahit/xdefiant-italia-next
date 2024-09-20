19bac3adc97595239549b8d2d5e22cb6b18c4615
Elijah <64620855+elijahit@users.noreply.github.com>
2024-09-20 22:30:34 +0200

    ADD: Pagination in news

--- src/app/api/postNews/route.js
+++ src/app/api/postNews/route.js
@@ -4,7 +4,7 @@ import db from "../../../../scripts/database";
 // To handle a GET request to /api
 export async function GET(request) {
   const searchParams = request.nextUrl.searchParams
-  const pagination = searchParams.get('pag') ? searchParams.get('pag') : 1;
+  const pagination = searchParams.get('pag') ? searchParams.get('pag') : 0;
   if(searchParams.get('limit')) {
     let query = searchParams.get('limit')
     let res = await db.all("select a.id_article , a.titolo, a.testo, a.id_utente, a.image_url, a.uri_article, u.username, u.email, u.discord_name, a.created_at from article a join utente u on u.id_utente = a.id_utente WHERE isApproved = ? ORDER BY a.id_article DESC LIMIT ? OFFSET ?", 1, query, +pagination)
